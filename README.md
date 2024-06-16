1da8413cf96744858e59fa57d1f16943d1a27e68
Ganeshkadali7337 <ganeshkadali0@gmail.com>
2024-06-16 16:13:45 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Weather Application
+
+This is a simple weather application that fetches and displays weather information for a given location using the OpenWeather API. The application includes the following features:
+
+- Toggle between the dark mode and light mode
+- Input field for search the weather condition by city name or ZIP code
+- Display current temperature
+- Display date and time of weather information
+- Show atmospheric pressure in hPa
+- Show visibility in km
+- Show humidity percentage
+- Display sunrise and sunset times
+- A loader will be displayed while it was loading
+- Shows an error message in alert for submitting any empty input fields or invalind inputs
+
+## Technologies Used
+
+- React: For building the user interface
+- OpenWeather API: For fetching weather data
+- CSS: For styling the application
+
+## Dependencies
+
+The project uses the following dependencies:
+
+- **axios**: For making API requests.
+- **react-icons**: For importing icons like sunrise sunset etc.
+- **react-loader-spinner**: To display the loader spinner while fetching data
+
+### Usage
+
+1. **Search by City Name**:
+
+   - Enter the name of the city in the input field.
+   - Click the "Get Weather" button to fetch and display the weather information.
+
+2. **Search by ZIP Code**:
+   - Enter the ZIP code and country code (eg: `94040,us`) in the two input fields.
+   - Click the "Get Weather" button to fetch and display the weather information.
+
+## Date and Time Formatting
+
+The application uses JavaScript's Date object to format date and time in the following ways:
+
+- **Sunrise and Sunset Times**: Displayed only time using `toLocaleTimeString` method.
+- **Date and Time of Weather**: Displayed the full date and time using `toLocaleString` method.
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
---
+++ src/Home/index.js
@@ -0,0 +1,320 @@
+import { Component } from "react";
+import { MdLightMode, MdDarkMode } from "react-icons/md";
+import { IoWater } from "react-icons/io5";
+import { BsFillSunriseFill, BsFillSunsetFill } from "react-icons/bs";
+import { LuWind } from "react-icons/lu";
+import { FaEye } from "react-icons/fa";
+import { PiThermometerHotFill } from "react-icons/pi";
+import { FaLocationDot } from "react-icons/fa6";
+import { TailSpin } from "react-loader-spinner";
+import axios from "axios";
+import "./index.css";
+
+class Home extends Component {
+  state = {
+    lightMode: true,
+    searchByCity: true,
+    city: "",
+    country: "",
+    zipCode: "",
+    weatherData: null,
+    isLoading: false,
+  };
+
+  onClickLightMode = () => {
+    this.setState({ lightMode: false });
+  };
+
+  onClickDarkMode = () => {
+    this.setState({ lightMode: true });
+  };
+
+  onClickZip = () => {
+    this.setState({ searchByCity: false });
+  };
+
+  onClickCity = () => {
+    this.setState({ searchByCity: true });
+  };
+
+  onChangeCity = (e) => {
+    this.setState({ city: e.target.value });
+  };
+
+  onChangeCountry = (e) => {
+    this.setState({ country: e.target.value });
+  };
+
+  onChangeZip = (e) => {
+    this.setState({ zipCode: e.target.value });
+  };
+
+  getDataByCity = async () => {
+    const { city } = this.state;
+    await axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=6a02b54f827d7181d2e0ad5bd1e49d78`
+      )
+      .then((res) => {
+        console.log(res);
+        this.setState({ weatherData: res.data, isLoading: false });
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({ isLoading: false });
+        alert(err.response.data.message);
+      });
+  };
+
+  getDataByZip = async () => {
+    const { zipCode, country } = this.state;
+    await axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},${country}&units=metric&appid=6a02b54f827d7181d2e0ad5bd1e49d78`
+      )
+      .then((res) => {
+        console.log(res);
+        this.setState({ weatherData: res.data, isLoading: false });
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({ isLoading: false });
+        alert(err.response.data.message);
+      });
+  };
+
+  onClickCitySearch = () => {
+    const { city } = this.state;
+    if (city.length > 0) {
+      this.setState({ isLoading: true, weatherData: null }, this.getDataByCity);
+    }
+  };
+
+  onClickZipSearch = () => {
+    const { zipCode, country } = this.state;
+    if (zipCode.length < 0 || country.length < 0) {
+      alert("Please provide valid zipcode and country code");
+    }
+    if (country.length !== 2) {
+      alert("please provide valid 2 letter country code (eg: IN)");
+    } else {
+      this.setState({ isLoading: true, weatherData: null }, this.getDataByZip);
+    }
+  };
+
+  renderInputSection = () => {
+    const { lightMode, searchByCity, city, country, zipCode } = this.state;
+
+    return (
+      <div>
+        {searchByCity ? (
+          <div className="input-container">
+            <div className="input-container2">
+              <label className="label" htmlFor="city">
+                City Name:
+              </label>
+              <input
+                value={city}
+                onChange={this.onChangeCity}
+                placeholder="Enter city name"
+                id="city"
+                className={lightMode ? "input-el" : `input-el input-dark`}
+                type="search"
+              />
+            </div>
+            <button onClick={this.onClickCitySearch} className="search-btn">
+              Get Weather
+            </button>
+            <p>OR</p>
+            <button className="search-btn" onClick={this.onClickZip}>
+              Search By Zip Code
+            </button>
+          </div>
+        ) : (
+          <div className="input-container">
+            <div className="input-container2 input-container-zip">
+              <div className="input-container2">
+                <label className="label-zip" htmlFor="zip-code">
+                  Zip Code:
+                </label>
+                <input
+                  value={zipCode}
+                  onChange={this.onChangeZip}
+                  placeholder="Enter zip code"
+                  id="zip-code"
+                  className={
+                    lightMode
+                      ? "input-el input-el-zip"
+                      : `input-el input-el-zip input-dark`
+                  }
+                  type="search"
+                />
+              </div>
+              <div className="input-container2">
+                <label className="label-zip" htmlFor="country-code">
+                  Country Code:
+                </label>
+                <input
+                  value={country}
+                  onChange={this.onChangeCountry}
+                  placeholder="Enter country code"
+                  id="country-code"
+                  className={
+                    lightMode
+                      ? "input-el input-el-zip"
+                      : `input-el input-el-zip input-dark`
+                  }
+                  type="search"
+                />
+              </div>
+            </div>
+            <button onClick={this.onClickZipSearch} className="search-btn">
+              Get Weather
+            </button>
+            <p>OR</p>
+            <button className="search-btn" onClick={this.onClickCity}>
+              Search By City
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  };
+
+  render() {
+    const { lightMode, isLoading, weatherData } = this.state;
+
+    return (
+      <div className={lightMode ? `bg-container light` : `bg-container dark`}>
+        <div className="content">
+          {lightMode ? (
+            <button className="toggle-button" onClick={this.onClickLightMode}>
+              <div className="mode-icon-container">
+                <MdLightMode className="mode-icon" />
+              </div>
+              Light Mode
+            </button>
+          ) : (
+            <button
+              className="toggle-button dark-toggle-btn"
+              onClick={this.onClickDarkMode}
+            >
+              Dark Mode
+              <div className="mode-icon-container">
+                <MdDarkMode className="mode-icon" />
+              </div>
+            </button>
+          )}
+          {this.renderInputSection()}
+          <div className="weather-data-container">
+            {isLoading && (
+              <TailSpin
+                height="55"
+                width="55"
+                color="#33b249"
+                ariaLabel="tail-spin-loading"
+                radius="1"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+              />
+            )}
+            {weatherData && (
+              <div className="data-cards-container">
+                <h1 className="city-name">
+                  {weatherData.name} <FaLocationDot className="location-icon" />
+                </h1>
+                <p className="date-para">
+                  Date and Time:{" "}
+                  {new Date(weatherData.dt * 1000).toLocaleString()}
+                </p>
+                <p className="temp-para">Temperature:</p>
+                <h2 className="temp">{weatherData.main.temp} °C</h2>
+                <ul className="cards-list">
+                  <li
+                    className={
+                      lightMode
+                        ? `list-item-card`
+                        : `list-item-card list-card-dark`
+                    }
+                  >
+                    <PiThermometerHotFill className="icon" />
+                    <p className="para2">Feels like</p>
+                    <p className="para3">{weatherData.main.feels_like} °C</p>
+                  </li>
+                  <li
+                    className={
+                      lightMode
+                        ? `list-item-card`
+                        : `list-item-card list-card-dark`
+                    }
+                  >
+                    <IoWater className="icon" />
+                    <p className="para2">Humidity</p>
+                    <p className="para3">{weatherData.main.humidity} %</p>
+                  </li>
+                  <li
+                    className={
+                      lightMode
+                        ? `list-item-card`
+                        : `list-item-card list-card-dark`
+                    }
+                  >
+                    <LuWind className="icon" />
+                    <p className="para2">Air Pressure</p>
+                    <p className="para3">{weatherData.main.pressure} hpa</p>
+                  </li>
+                  <li
+                    className={
+                      lightMode
+                        ? `list-item-card`
+                        : `list-item-card list-card-dark`
+                    }
+                  >
+                    <FaEye className="icon" />
+                    <p className="para2">Visibility</p>
+                    <p className="para3">
+                      {Math.round(weatherData.visibility / 1000)} km
+                    </p>
+                  </li>
+                  <li
+                    className={
+                      lightMode
+                        ? `list-item-card`
+                        : `list-item-card list-card-dark`
+                    }
+                  >
+                    <BsFillSunriseFill className="icon" />
+                    <p className="para2">Sunrise</p>
+                    <p className="para3">
+                      {new Date(
+                        weatherData.sys.sunrise * 1000
+                      ).toLocaleTimeString()}
+                    </p>
+                  </li>
+                  <li
+                    className={
+                      lightMode
+                        ? `list-item-card`
+                        : `list-item-card list-card-dark`
+                    }
+                  >
+                    <BsFillSunsetFill className="icon" />
+                    <p className="para2">Sunset</p>
+                    <p className="para3">
+                      {new Date(
+                        weatherData.sys.sunset * 1000
+                      ).toLocaleTimeString()}
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
